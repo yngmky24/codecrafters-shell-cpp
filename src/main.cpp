d8e5f6b7af3b852ef15d21215479290c23f5423d
Alvin Young <100006273+yngmky24@users.noreply.github.com>
2026-05-20 11:21:12 +1000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib>
+
+// List of builtin commands
+std::vector<std::string> builtin {
+  "echo",
+  "exit",
+  "type"
+};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -16,6 +24,19 @@ int main() {
     }
     else if (userInput.substr(0, 5) == "echo ") {
       std::cout << userInput.substr(5) << '\n';
+    }
+    else if (userInput.substr(0, 5) == "type ") {
+      bool is_builtin = false;
+      for(int index = 0; index < builtin.size(); index++) {
+        if (userInput.substr(5) == builtin[index]) {
+          std::cout << builtin[index] << " is a shell builtin\n";
+          is_builtin = true;
+          break;
+        }
+      }
+      if (!is_builtin) {
+        std::cout << userInput.substr(5) << ": not found\n";
+      }
     }
     else {
       std::cout << userInput << ": command not found\n";
