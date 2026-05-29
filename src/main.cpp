bf0fc634e38eaab47afdf2b29b1f2143ce5b2e29
Alvin Young <100006273+yngmky24@users.noreply.github.com>
2026-05-29 11:40:25 +1000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,17 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-
-// List of builtin commands
-std::vector<std::string> builtin {
-  "echo",
-  "exit",
-  "type"
-};
+#include <sstream>
+#include <filesystem>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  // Initialize an object to hold user inputs.
   std::string userInput {};
+  // Loop infinitely until user calls `exit` command.
   while(1) {
     std::cout << "$ ";
     std::getline(std::cin, userInput);
@@ -23,23 +20,37 @@ int main() {
       exit(0);
     }
     else if (userInput.substr(0, 5) == "echo ") {
-      std::cout << userInput.substr(5) << '\n';
+      std::cout << userInput.substr(5) << std::endl;
     }
     else if (userInput.substr(0, 5) == "type ") {
       bool is_builtin = false;
-      for(int index = 0; index < builtin.size(); index++) {
+      // List of builtin commands
+      std::string builtin[3] {"echo", "exit", "type"};
+      for(int index = 0; index < builtin->length(); index++) {
         if (userInput.substr(5) == builtin[index]) {
-          std::cout << builtin[index] << " is a shell builtin\n";
+          std::cout << builtin[index] << " is a shell builtin" << std::endl;
           is_builtin = true;
           break;
         }
       }
       if (!is_builtin) {
-        std::cout << userInput.substr(5) << ": not found\n";
+        std::string path_env = std::getenv("PATH");
+        std::stringstream ss_path(path_env);
+        std::string path;
+        while(std::getline(ss_path, path, ':')) {
+          std::filesystem::path p{path};
+          std::filesystem::path fullPath = p / userInput.substr(5);
+          if(std::filesystem::exists(fullPath)) {
+            std::cout << userInput.substr(5) << "is" << fullPath;
+            is_builtin = true;
+            break;
+          }
+        }
+        std::cout << userInput.substr(5) << ": not found" << std::endl;
       }
     }
     else {
-      std::cout << userInput << ": command not found\n";
+      std::cout << userInput << ": command not found" << std::endl;
     }
   }
 }
