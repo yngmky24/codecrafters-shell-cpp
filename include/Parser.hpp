e207be09db346bafde549bf28066e6202bfd79bd
Alvin Young <100006273+yngmky24@users.noreply.github.com>
2026-06-02 22:52:26 +1000

    codecrafters submit [skip ci]

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -2,14 +2,14 @@
 #include <vector>
 #include <string>
 
+enum class ParseState {
+  Normal,
+  // NormalAfterSpace,
+  InSingleQuote,
+  InDoubleQuote
+};
+
 class Parser {
-private:
-  enum class ParseState {
-    Normal,
-    NormalAfterSpace,
-    InSingleQuote,
-    InDoubleQuote
-  };
 public:
   std::vector<std::string> parseLine(const std::string& line);
 };
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,4 +1,5 @@
 #include "../include/Parser.hpp"
+#include <ios>
 #include <sstream>
 
 std::vector<std::string> Parser::parseLine(const std::string& line) {
@@ -10,50 +11,89 @@ std::vector<std::string> Parser::parseLine(const std::string& line) {
   char c {};    
   
   // Loop through all characters
-  while(ss >> std::noskipws >> c) {
-    if (c == '\'' && (state==ParseState::Normal || state==ParseState::NormalAfterSpace)) {
-      // Begin single quote
-      state = ParseState::InSingleQuote;
-    }
-    else if (c == '\'' && state==ParseState::InSingleQuote) { 
-      // End single quote          
-      state = ParseState::Normal;       
-    }
-    else if (state==ParseState::InSingleQuote) { 
-      // Between single quotes
-      currentToken += c;
-    }
-    else if (c == '\"' && (state==ParseState::Normal || state==ParseState::NormalAfterSpace)) {
-      // Begin double quote
-      state = ParseState::InDoubleQuote;
-    }
-    else if (c == '\"' && state==ParseState::InDoubleQuote) {
-      // End of double quote
-      state = ParseState::Normal;
-    }
-    else if (state==ParseState::InDoubleQuote) {
-      currentToken += c;
-    }
-    else if (state==ParseState::Normal) {
-      if (c == ' ') { // The end of a word
-        state = ParseState::NormalAfterSpace;
-        // Add the word to the list
-        if (!currentToken.empty()) {
-          tokens.push_back(currentToken);
-          currentToken.clear();
-        }       
-      }
-      else {            
-        currentToken += c;
-      }
-    }
-    else if (state==ParseState::NormalAfterSpace) {
-      if (c != ' ') {
-        state = ParseState::Normal;
-        currentToken += c;
-      }
+  // while(ss >> std::noskipws >> c) {
+  //   if (c == '\'' && (state==ParseState::Normal || state==ParseState::NormalAfterSpace)) {
+  //     // Begin single quote
+  //     state = ParseState::InSingleQuote;
+  //   }
+  //   else if (c == '\'' && state==ParseState::InSingleQuote) { 
+  //     // End single quote          
+  //     state = ParseState::Normal;       
+  //   }
+  //   else if (state==ParseState::InSingleQuote) { 
+  //     // Between single quotes
+  //     currentToken += c;
+  //   }
+  //   else if (c == '\"' && (state==ParseState::Normal || state==ParseState::NormalAfterSpace)) {
+  //     // Begin double quote
+  //     state = ParseState::InDoubleQuote;
+  //   }
+  //   else if (c == '\"' && state==ParseState::InDoubleQuote) {
+  //     // End of double quote
+  //     state = ParseState::Normal;
+  //   }
+  //   else if (state==ParseState::InDoubleQuote) {
+  //     currentToken += c;
+  //   }
+  //   else if (state==ParseState::Normal) {
+  //     if (c == ' ') { // The end of a word
+  //       state = ParseState::NormalAfterSpace;
+  //       // Add the word to the list
+  //       if (!currentToken.empty()) {
+  //         tokens.push_back(currentToken);
+  //         currentToken.clear();
+  //       }       
+  //     }
+  //     else {            
+  //       currentToken += c;
+  //     }
+  //   }
+  //   else if (state==ParseState::NormalAfterSpace) {
+  //     if (c != ' ') {
+  //       state = ParseState::Normal;
+  //       currentToken += c;
+  //     }
+  //   }
+  // }
+
+  while (ss >> std::noskipws >> c) {
+    switch (state) {
+      case ParseState::Normal:
+        if (c == '\'') {
+          state = ParseState::InSingleQuote;
+        }
+        else if (c == '\"') {
+          state = ParseState::InDoubleQuote;
+        }
+        else if (c == ' ') {
+          if(!currentToken.empty()) {
+            tokens.push_back(currentToken);
+            currentToken.clear();
+          }
+        }
+        else {
+          currentToken += c;
+        }
+        break;
+      case ParseState::InSingleQuote:
+        if (c == '\'') {
+          state = ParseState::Normal;          
+        }
+        else {
+          currentToken += c;
+        }
+        break;
+      case ParseState::InDoubleQuote: 
+        if (c == '\"') {
+          state = ParseState::Normal;
+        }
+        else {
+          currentToken += c;
+        }
+        break;
     }
   }
+
   // Catch the last word if the line didn't end with a space
   if (!currentToken.empty()) {
     tokens.push_back(currentToken);
